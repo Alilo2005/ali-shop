acf7b97b8d3c272c5b80f5acb5571170872b730a
Ali Mahdi <ali.mahdi@example.com>
2025-08-09 01:26:40 +0100

    ✨ Refactor Providers component to remove ThemeProvider; add loading components for improved structure

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,16 +2,13 @@
 
 import { SessionProvider } from 'next-auth/react'
 import { Suspense } from 'react'
-import { ThemeProvider } from '@/components/theme-provider'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <ThemeProvider defaultTheme="light" storageKey="ali-shop-theme">
-        <Suspense fallback={<div>Loading...</div>}>
-          {children}
-        </Suspense>
-      </ThemeProvider>
+      <Suspense fallback={<div>Loading...</div>}>
+        {children}
+      </Suspense>
     </SessionProvider>
   )
 }
